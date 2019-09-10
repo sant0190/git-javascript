f027d5d7b6865cf11efda35d5cf81303a4300b6e
Christine Grace Santos <sant0190@algonquinlive.com>
2019-09-10 19:58:39 -0400

    adds script to Git JavaScript

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const name1 = `Christine Grace Santos`
+const name = `Christine Grace Santos`
 const $number = 3
 
-console.log(`Hello, my name is ${name1}`)
+console.log(`Hello, my name is ${name}`)
 console.log($number * 2 * 5 / $number - 7)
